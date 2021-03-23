767ce206b0885bfbb1252cd06a64674c0061a54d
mohamed <rachidmohamed072@gmail.com>
2021-03-23 13:00:14 +0100

    fdfhfdh

--- libreria/apilibreria/src/Config/config.php
+++ libreria/apilibreria/src/Config/config.php
@@ -3,9 +3,9 @@ namespace App\Config;
 
 interface config {
     CONST DB_NAME    = "libreria";
-    CONST DB_USER    = "mohamed";
+    CONST DB_USER    = "root";
     CONST DB_PORT    = "3306";
-    CONST DB_PASS    = "Mohamed1.*";
+    CONST DB_PASS    = "";
     CONST DB_CHARSET = "utf8";
     CONST DB_HOST    = "localhost";
 }
