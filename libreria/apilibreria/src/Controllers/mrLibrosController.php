a89ccaedad590f1a44ec5fda17d68412e10eef15
mohamed <rachidmohamed072@gmail.com>
2021-03-24 19:01:58 +0100

    php.c

--- libreria/apilibreria/src/Controllers/mrLibrosController.php
+++ libreria/apilibreria/src/Controllers/mrLibrosController.php
@@ -7,27 +7,8 @@
 
     class mrLibrosController {
         
-        public function new($request, $response, $args){
-            $response->getBody()->write("Insertar un nuevo Libro");
-            return $response
-                ->withHeader('Content-Type', 'application/json')
-                ->withStatus(200);
-        }
-        public function show(Request $request, Response $response, $args){
-          var_dump($args);
-          $id = $args['id'];
-          $ciudad = $args['ciudad'];
-          echo $id." ".$ciudad;    
-        }
         
-
-        public function getFilter(Request $request, Response $response, $args){
-            $parametros = $request->getQueryParams();
-            var_dump($parametros);
-            $precio = $parametros['precio'];
-            $nombre = $parametros['nombre_libro'];
-            echo $nombre. " " .$precio;
-        }
+       
         
         public function mrAll($request, $response, $args){
             $libros = mrLibrosModel::mrAll();
--- libreria/apilibreria/src/Model/mrLibrosModel.php
+++ libreria/apilibreria/src/Model/mrLibrosModel.php
@@ -9,23 +9,15 @@ class mrLibrosModel {
     public static function conexionDB(){
         mrLibrosModel::$DB = new DB();
     }
-    public static function getFilter($sql, $param){
-        $data = mrLibrosModel::$DB->run($sql, $param);
-        return $data->fetchAll();
-    }
+   
 
     public static function mrAll(){
         mrLibrosModel::conexionDB();
-        $sql = "Select * from libros";
+        $sql = "Select * from libros ";
         $data = mrLibrosModel::$DB->run($sql, []);
         return $data->fetchAll();
     }
-    public static function show($param){
-        LibrosModel::conexionDB();
-        $sql = 'SELECT * from libros where libro_id = ?';
-        $data = LibrosModel::$DB->run($sql, $param);
-        return $data->fetch();
-    }
+   
     
   
 }
