2e2df004f0970e1eae389d96561943ebd134d5cf
mohamed <rachidmohamed072@gmail.com>
2021-03-24 19:08:19 +0100

    phph

--- libreria/apilibreria/src/Routes/mrlibros.php
+++ libreria/apilibreria/src/Routes/mrlibros.php
@@ -1,10 +1,4 @@
 <?php
 # para agrupar las rutas en grupos
 use Slim\Routing\RouteCollectorProxy;
-
-$app->group('/api', function(RouteCollectorProxy $group){
-    $group->get('/mrlibros', 'App\Controllers\mrLibrosController:mrAll');
-    $group->get('/libros/show/{id}/{ciudad}', 'App\Controllers\LibrosController:show');
-    
-    #  $group->get('/libros/{id}', 'App\Controllers\LibrosController:show');
-});
+$app->get('/mrlibros', 'App\Controllers\mrLibrosController:mrAll');    
